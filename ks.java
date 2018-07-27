0fa5c44e252c47cd226e62c6ac141c788a512c16
kollurisireesha <41730733+kollurisireesha@users.noreply.github.com>
2018-07-27 13:11:17 +0530

    Create ks.java

---
+++ ks.java
@@ -0,0 +1,33 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class ks
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int  n=sc.nextInt();
+		int  k=sc.nextInt();
+		int sum=0;
+		int i;
+		int n1;
+		for(i=1;i<=n;i++)
+		{
+			n1=sc.nextInt();
+		}
+		for(i=1;i<=k;i++)
+		{
+			sum=sum+i;
+		}
+		System.out.println(sum);
+	}
+
+		
+		
+		
+		// your code goes here
+	}
