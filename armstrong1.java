7976ef61b44b9c83a3e01bf6faa78895d138326e
kollurisireesha <41730733+kollurisireesha@users.noreply.github.com>
2018-07-27 14:47:59 +0530

    Create armstrong1.java

---
+++ armstrong1.java
@@ -0,0 +1,32 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class armstrong
+
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+
+        int number = 153, originalNumber, remainder, result = 0;
+
+        originalNumber = number;
+
+        while (originalNumber != 0)
+        {
+            remainder = originalNumber % 10;
+            result += Math.pow(remainder, 3);
+            originalNumber /= 10;
+        }
+
+        if(result == number)
+            System.out.println(number + " is an Armstrong number.");
+        else
+            System.out.println(number + " is not an Armstrong number.");
+    }
+}
+		// your code goes here
+	
