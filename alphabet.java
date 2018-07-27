43740fbc57ac8bda1b2ef8710cee4443d5a861b6
kollurisireesha <41730733+kollurisireesha@users.noreply.github.com>
2018-07-27 10:39:08 +0530

    Create alphabet.java

---
+++ alphabet.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class alphabet
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+
+        char c = 'A';
+        
+        String output = (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+                ? c + " is an alphabet."
+                : c + " is not an alphabet.";
+        
+        System.out.println(output);
+    }
+}
+		// your code goes here
+	
