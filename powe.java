f18e85e57cdc9516bda7b9a4304c87bc03520451
kollurisireesha <41730733+kollurisireesha@users.noreply.github.com>
2018-07-27 13:20:17 +0530

    Create powe.java

---
+++ powe.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class power
+{
+
+    public static void main(String[] args) {
+
+        int base = 3, exponent = 4;
+
+        long result = 1;
+
+        while (exponent != 0)
+        {
+            result *= base;
+            --exponent;
+        }
+
+        System.out.println("Answer = " + result);
+    }
+}
+
+		
+		// your code goes here
+	
