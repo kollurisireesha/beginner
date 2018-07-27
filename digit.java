78e0b4d2635ae87286214353dade8a88afec2bc4
kollurisireesha <41730733+kollurisireesha@users.noreply.github.com>
2018-07-27 12:52:35 +0530

    Create digit.java

---
+++ digit.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class digit
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int count = 0, num = 3452;
+
+        while(num != 0)
+        {
+            // num = num/10
+            num /= 10;
+            ++count;
+        }
+
+        System.out.println("Number of digits: " + count);
+    }
+}
+		// your code goes here
